324e741fd8177caeb23114d1fa9b1cc65ec86814
CMHend <cmilesh@live.com>
2022-02-03 08:29:51 -0600

    Set up to do list lab

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const toDoInput = document.getElementById('to-do')
+const submitBtn = document.getElementById('submit-button')
+const toDoList = document.getElementById('todo-list')
+
+submitBtn.addEventListener('click', evt => {
+  const li = document.createElement('li')
+  
+}
+)
